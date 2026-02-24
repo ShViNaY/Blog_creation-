f85a89fe0fb769ba54f42225de21cd118bf539db
Vini <Vini@gmail.com>
2026-02-25 01:05:51 +0530

    Add user authentication (register, login, logout), profile model, and admin configuration

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import  render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -24,5 +25,6 @@ def logout_view(request):
     logout(request)
     return render(request, 'users/logout.html')
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
