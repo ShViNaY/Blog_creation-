3d39393a9d0c3012763cbdc261af5c697cf39818
Vini <Vini@gmail.com>
2026-02-24 19:55:22 +0530

    add gitignore

--- myproject/urls.py
+++ myproject/urls.py
@@ -9,8 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('profile/', user_views.profile, name='profile'),
     path('logout/', user_views.logout_view, name='logout'),
 
     path('', include('blog.urls')),
